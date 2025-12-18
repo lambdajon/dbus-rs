77e86158ba11c2b9625dcb57bdd1a70e6d82cf9c
lambdajon <lambdajon42@gmail.com>
2025-12-18 12:09:30 +0500

    wip

--- dbus/src/arg/array_impl.rs
+++ dbus/src/arg/array_impl.rs
@@ -595,8 +595,10 @@ pub fn get_array_refarg(i: &mut Iter) -> Box<dyn RefArg> {
             let key = ArgType::from_i32(i.signature().as_bytes()[2] as i32).unwrap(); // The third character, after "a{", is our key.
             // let values_as_bytes = i.clone().signature().as_bytes().iter().map(|x: &u8| (x.as_str()));
             // let values_as_bytes = i
-            let rrr = i.signature().as_bytes()[3];
-            println!("FuckingVal: {:?}",rrr);
+            // let rrr = i.signature().as_bytes()[3];
+            let rrr2 = i.signature().as_bytes()[4];
+            // println!("FuckingVal: {:?}",rrr);
+            println!("FuckingVal: {:?}",rrr2);
 
             let value = ArgType::from_i32(i.signature().as_bytes()[3] as i32).unwrap(); // The fourth character, after "a{", is our value.
             

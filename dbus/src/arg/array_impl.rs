723133093a465910944a8b10356c2763ea3362d9
lambdajon <lambdajon42@gmail.com>
2025-12-18 11:41:18 +0500

    wip

--- dbus/src/arg/array_impl.rs
+++ dbus/src/arg/array_impl.rs
@@ -593,11 +593,12 @@ pub fn get_array_refarg(i: &mut Iter) -> Box<dyn RefArg> {
         ArgType::Array => get_internal_array(i),
         ArgType::DictEntry => {
             let key = ArgType::from_i32(i.signature().as_bytes()[2] as i32).unwrap(); // The third character, after "a{", is our key.
-            let values_as_bytes = i.clone().signature().as_bytes().iter().map(|x: &u8| (x.as_str()));
+            // let values_as_bytes = i.clone().signature().as_bytes().iter().map(|x: &u8| (x.as_str()));
+            // let values_as_bytes = i
             let value = ArgType::from_i32(i.signature().as_bytes()[3] as i32).unwrap(); // The fourth character, after "a{", is our value.
             
-            println!("DictEntry: {:?}", i.signature().bytes());
-            println!("ValuesMap: {:?}", values_as_bytes);
+            // println!("DictEntry: {:?}", i.signature().bytes());
+            println!("Message: {:?}", i.1);
             match key {
                 ArgType::Byte => get_dict_refarg_for_value_type::<u8, _>(value, i, Iter::get),
                 ArgType::Int16 => get_dict_refarg_for_value_type::<i16, _>(value, i, Iter::get),
